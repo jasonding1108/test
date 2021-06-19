c92d09ffbc359ef2c4e7cf6195992ade62b08ef0
unknown <a9102107@hotmail.com>
2021-06-19 21:44:58 +0800

    add new file

---
+++ 123.py
@@ -0,0 +1,24 @@
+# change path command: 
+#cd /d D:\Python\conding_study
+
+
+x=5
+print(5)
+
+
+#learn data types
+#1. int
+#2. float
+#3. bool   (True, False)
+#4. str    ('Allen') 
+price = 10
+pen = 0.38
+rain = True
+name = 'Allen'
+print(price, pen, rain, name)
+
+
+#learn input
+name = input('input name: ')  #stop program, wait user's input
+print('hi', name)
+
