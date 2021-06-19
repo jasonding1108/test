39eda4092d6b966e8f30d70d371cdb5a0fc84a5f
unknown <a9102107@hotmail.com>
2021-06-19 22:30:30 +0800

    c to f program

---
+++ ctof.py
@@ -0,0 +1,9 @@
+
+#user input C transfer to F
+#F = C * 9/5 + 32
+
+temp_c = input ('please input temperature (degree c):')
+temp_c_int = float(temp_c)
+
+temp_f = temp_c_int * 9/5 + 32
+print('degree f is', temp_f)
