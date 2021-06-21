b960002f615627ba058afbe7fd51959703fcc8dd
unknown <a9102107@hotmail.com>
2021-06-21 23:22:59 +0800

    add new function

--- readmessage.py
+++ readmessage.py
@@ -1,4 +1,8 @@
 
+#每1000筆印出一筆數字
+#印出data數量: 共有100萬筆
+#印出100萬筆資料的平均長度
+
 data = []
 count = 0
 sum_len = 0
@@ -13,12 +17,18 @@ with open('reviews.txt', 'r') as f:
 		length = len(line)
 		sum_len = sum_len + length
 		data_num = len(data)
-
 		#print(sum_len)
 		avg_len = sum_len / data_num
 	
-
 print('read finish, total are', len(data), 'data')
 print('average length is', avg_len)
 
 
+#篩選長度<100
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('the number of message length < 100:', len(new))
+print(new[0])
+print(new[1])
